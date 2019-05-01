f5211c50c4216f58cb60ea4461a9c98aae130e4e
Rasika Amaratissa <ramaratissa@infoxchange.org>
2019-05-01 15:18:39 +1000

    Add Python setup file

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name='python_graphql_client',
+    version='0.1',
+    description='A Python client to consume a GraphQL endpoint.',
+    url='https://github.com/infoxchange/python-graphql-client',
+    author='Infoxchange Developers',
+    author_email='developers@infoxchange.org',
+    license='MIT',
+    packages=setuptools.find_packages(),
+    install_requires=[],
+    zip_safe=False
+)
